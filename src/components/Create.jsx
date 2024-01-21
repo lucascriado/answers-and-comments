6fd34f48e24a695e33b73e3ce5852feeeae3b044
lucascriado <lucaasbrandini@gmail.com>
2024-01-20 23:42:03 -0300

    feat: criando sistema de registro de postagens

---
+++ src/components/Create.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+export function CreatePost() {
+  const [author, setAuthor] = useState('');
+  const [content, setContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const post = {
+      author: {
+        avatarUrl: 'https://github.com/diego3g.png',
+        name: author,
+        role: 'CTO @Rocketseat'
+      },
+      content: [
+        { type: 'paragraph', content: content },
+      ],
+      publishedAt: new Date().toISOString()
+    };
+
+    fetch('http://localhost:3001/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(post),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Success:', data);
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Author:
+        <input type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+      </label>
+      <label>
+        Content:
+        <textarea value={content} onChange={e => setContent(e.target.value)} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+}
